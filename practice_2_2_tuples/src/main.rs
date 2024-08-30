3bddca783d8ffc0e73b7741a35a168e4c797eb2f
motw <motw@yandex.ru>
2024-08-30 07:38:28 +0400

    activity for chapter 2.2.

--- practice_2_2_tuples/src/main.rs
+++ practice_2_2_tuples/src/main.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 // Tuples can be used as function arguments and as return values.
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     // `let` can be used to bind the members of a tuple to variables.
@@ -10,6 +12,16 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
 #[allow(dead_code)]
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
 
 fn main() {
     // A tuple with a bunch of different types.
@@ -51,4 +63,6 @@ fn main() {
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
     println!("{:?}", matrix);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
