88dc9502c8502b4f6fa6c865325ea85f9c9b9e1e
motw <motw@yandex.ru>
2024-08-30 17:11:51 +0400

    add 6. Conversion

---
+++ practice_6_1_from_and_into/src/main.rs
@@ -0,0 +1,30 @@
+#![allow(dead_code)]
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+// impl Into<Number> for i32 {
+//     fn into(self) -> Number {
+//         Number { value: self }
+//     }
+// }
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    // use `Into`
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
