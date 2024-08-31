c7090caa7c5cb27151856dd7ae7f7056081fec06
motw <motw@yandex.ru>
2024-08-31 12:00:15 +0400

    add rest of 8. Flow of Control

---
+++ practice_8_2_1_nesting_and_labels/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unreachable_code, unused_labels)]
+
+fn main() {
+    #[allow(clippy::never_loop)]
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
